2af19aa0b2fdbfb508159037737be35a9fdc824d
Packlayman1 <packlayman1@gmail.com>
2021-04-08 16:42:46 +0700

    กำหนดหมายเลขหน้าแบบ Query Builder

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\DB;
 class DepartmentController extends Controller
 {
     public function index(){
-        $departments = Department::paginate(3);
+        // $departments = Department::paginate(3);
+        $departments = DB::table('departments')->paginate(3);
         // $departments = Department::all();
         // $departments = DB::table('departments')->get();
         return view('admin.department.index',compact('departments'));
