f9d90a0aae7446e43b3906fa343d88cb6c2526a9
Packlayman1 <packlayman1@gmail.com>
2021-04-08 16:38:58 +0700

    กำหนดหมายเลขหน้าแบบ Query Builder

--- resources/views/admin/department/index.blade.php
+++ resources/views/admin/department/index.blade.php
@@ -16,6 +16,34 @@
                         <div class="card-header">
                             ตารางข้อมูลแผนก
                         </div>
+                        {{-- <table class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ลำดับ</th>
+                                     <th scope="col">ชื่อแผนก</th>
+                                    <th scope="col">UserID</th>
+                                    <th scope="col">created at</th> -
+                                </tr>
+                            </thead>
+                            <tbody >
+                                @foreach ($departments as $row)
+                                    <tr>
+                                        <th>{{$departments->firstItem()+$loop->index}}<th>
+                                         <td>{{$row->department_name}}</td>
+                                        <td>{{$row->user_id}}</td>
+                                        <td>
+                                            @if($row->created_at == NULL)
+                                            -
+                                             @else
+                                             {{Carbon\Carbon::parse($row->created_at)->diffForHumans()}}
+                                             @endif
+                                        </td> 
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table> --}}
+
+
                         <table class="table table-striped table-bordered">
                             <thead>
                                 <tr>
@@ -26,20 +54,23 @@
                                 </tr>
                             </thead>
                             <tbody >
-                                @php($i=1)
                                 @foreach ($departments as $row)
                                     <tr>
-                                        <th scope="row">{{$i++}}</th>
-                                        <td>{{$row->department_name}}</td>
-                                        <td>{{$row->user_id}}</td>
-                                        <td>
-                                            @if($row->created_at == NULL)
-                                            -
-                                             @else
-                                             {{Carbon\Carbon::parse($row->created_at)->diffForHumans()}}
-                                             @endif
-                                            {{-- {{Carbon\Carbon::parse($row->created_at)->diffForHumans()}} --}}
-                                        </td>
+                                        {{-- <td>1</td>
+                                        <td>2</td>
+                                        <td>3</td>
+                                        <td>4</td> --}}
+
+                                         <th>{{$departments->firstItem()+$loop->index}}<th> 
+                                         <td>{{$row->department_name}}</td>
+                                         <td>{{$row->user_id}}</td>
+                                           <td>
+                                               @if($row->created_at == NULL)
+                                                  -
+                                                @else
+                                                {{Carbon\Carbon::parse($row->created_at)->diffForHumans()}}
+                                                @endif
+                                           </td>  
                                     </tr>
                                 @endforeach
                             </tbody>
